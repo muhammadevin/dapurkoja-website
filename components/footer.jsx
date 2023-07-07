bbde9901fd211fb35ba9aa62e67706572ec665ba
Devin Pratista <devinpratista@Devins-MacBook-Air.local>
2023-07-07 21:12:02 +0700

    Edit footer responsiveness

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,26 +5,30 @@ import halal from '../public/halal.png'
 
 const Footer = () => {
   return (
-    <div className='h-[225px] flex flex-row px-16 py-10 justify-between bg-secondary'>
-      <div className='flex flex-col gap-4 items-start w-[344px]'>
-        <div className='flex items-center gap-2'>
-          <BsInstagram />
-          <p className='text-sm'>@dapur_koja</p>
-        </div>
-        <div className='flex items-center gap-2'>
-          <FiMail />
-          <p className='text-sm'>halo.dapurkoja@gmail.com</p>
-        </div>
+    <div className='pb-10 w-full gap-4 flex flex-col md:flex-row pt-8 px-4 py-4 md:px-16 md:py-10 bg-secondary justify-between'>
+      <div className='flex flex-col gap-4 items-start w-[164px] md:w-[188px]'>
+        <a href="https://www.instagram.com/dapur_koja/">
+          <div className='flex items-center gap-2'>
+            <BsInstagram />
+            <p className='text-xs md:text-sm'>@dapur_koja</p>
+          </div>
+        </a>
+        <a href="mailto:halo.dapurkoja@gmail.com">
+          <div className='flex items-center gap-2'>
+            <FiMail />
+            <p className='text-xs md:text-sm'>halo.dapurkoja@gmail.com</p>
+          </div>
+        </a>
       </div>
 
-      <div className='flex flex-col gap-4 text-center items-center w-[344px]'>
-        <h4 className='text-lg font-bold'>FRESHLY MADE BY ORDER EVERYDAY</h4>
-        <p className='text-lg font-semibold'>No MSG, No preservatives</p>
-        <p className='mt-auto'>© 2023 Dapur Koja. All rights reserved.</p>
+      <div className='flex flex-col gap-1 text-left md:text-center w-[344px]'>
+        <h4 className='text-xs md:text-lg font-bold'>FRESHLY MADE BY ORDER EVERYDAY</h4>
+        <p className='text-xs md:text-lg font-semibold'>No MSG, No preservatives</p>
+        <p className='text-xs md:text-base'>© 2023 Dapur Koja. All rights reserved.</p>
       </div>
 
-      <div className='flex flex-row w-[344px] justify-end'>
-        <Image src={halal} className='w-[84px] h-[84px]' />
+      <div className='flex w-[164px] md:w-[188px] justify-start md:justify-end'>
+        <Image src={halal} className="w-[32px] h-[32px] md:w-[64px] md:h-[64px] lg:w-[84px] lg:h-[84px]" />
       </div>
     </div>
   )
