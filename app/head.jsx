baf39ac6cde700fa3612144db49800158acd1f94
Devin Pratista <devinpratista@Devins-MacBook-Air.local>
2023-07-08 12:14:58 +0700

    Add header component with icon

--- app/head.jsx
+++ app/head.jsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Dapur Koja</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Dapur Koja" />
-      <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+      <link rel="icon" type="image/x-icon" href="/icon.ico"/>
     </head>
   )
 }
